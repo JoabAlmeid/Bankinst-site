36c42cc540113dda04185aa0dbf6db2db9507a72
Joab Almeida <joabalmeida.dev@gmail.com>
2025-04-29 22:52:02 -0300

    selecting elements

--- script.js
+++ script.js
@@ -29,3 +29,29 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+////////////////////////////////////////
+////////////////////////////////////////
+////////////////////////////////////////
+////////////////////////////////////////
+
+//  SELECTING ELEMENTS
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+//THESE TWO ARE THE MOST USEFUL
+//selects just the first header
+document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+//searches for the id in the HTML
+document.getElementById('section--1');
+
+//gets everything that is a button in HTML
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+//gets everything that has 'btn' written in class
+document.getElementsByClassName('btn');
