f2773d19c96ded2d46de7e1195c546e759455d91
Joab Almeida <joabalmeida.dev@gmail.com>
2025-05-02 23:42:58 -0300

    HTML attributes

--- script.js
+++ script.js
@@ -97,3 +97,39 @@ console.log(getComputedStyle(message).height);
 
 message.style.height =
   Number.parseFloat(getComputedStyle(message).height) + 40 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Atributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+console.log(logo.designer);
+
+logo.alt = 'Beautiful minimalist logo';
+
+// Non-stantard
+console.log(logo.designer);
+//can't show properties created, only official. But you can do it like that to show it
+console.log(logo.getAttribute('designer'));
+//you can even create properties in the HTML
+logo.setAttribute('company', 'Bankist');
+
+const link = document.querySelector('.nav__link--btn');
+//gets the absolute link
+console.log(link.href);
+//gets the link in the HTML
+console.log(link.getAttribute('href'));
+
+// Data attributes (every atribute that has 'data' written enters here)
+console.log(logo.dataset.versionNumber);
+
+// Classes (podemos criar classes nos elementos)
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c');
+
+//DONT USE (WILL OVERWRITE EVERY CLASS)
+logo.className = 'jonas';
