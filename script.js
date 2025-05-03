2de1fa2b359b7ee82594de5fb5004bc43cdb4952
Joab Almeida <joabalmeida.dev@gmail.com>
2025-05-02 22:37:51 -0300

    styles height pixel color

--- script.js
+++ script.js
@@ -84,3 +84,16 @@ document
     // message.remove();
     message.parentElement.removeChild(message);
   });
+
+// Styles //////
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height) + 40 + 'px';
